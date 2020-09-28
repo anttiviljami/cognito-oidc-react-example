8bb849a348867aae7ef7810f7d213d85654f9c9c
Viljami Kuosmanen <viljami.kuosmanen@futurice.com>
2020-09-28 17:11:02 +0200

    Simplify cognito.js

--- src/cognito.js
+++ src/cognito.js
@@ -1,13 +1,12 @@
 import React, { createContext, useContext, useState } from 'react';
 import { UserManager, WebStorageStateStore } from 'oidc-client';
 
-export const CognitoContext = createContext({});
+const CognitoContext = createContext({});
 
 const userManager = new UserManager({
   client_id: 'm5hcjoi5q7b4rpjgoben8eeic',
   authority: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_Eg6m7vHvR',
   redirect_uri: 'http://localhost:3000',
-  silent_redirect_uri: 'http://localhost:3000',
   response_type: 'code',
   scope: 'openid',
   userStore: new WebStorageStateStore({ store: window.localStorage }),
